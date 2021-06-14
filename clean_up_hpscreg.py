7eee06ec1b3e077911de2a0c5d83fda1cf6d4a85
lubianat <tiago.lubiana.alves@usp.br>
2021-06-14 09:59:38 -0300

    feat: Add clean up code for hPSCreg IDs

---
+++ clean_up_hpscreg.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python3
+
+
+"""
+Updates the Wikidata items relative to the cells in the Cellosaurus dump of interest. 
+
+It takes 3 arguments: 
+- 1st: The path to the .txt of the Cellosaurus dump
+- 2nd: The path to the folder where the pickle file and cell lines on wikidata 
+were saved after running "prepare_files.py"
+- 3rd: The folder for errors.
+- 4th: The QID for the Cellosaurus release on Wikidata   
+
+ Example:
+ $python3 update_wikidata.py project/test_cellosaurus.txt dev/pickle_files dev/errors Q87574023 
+ """
+
+from wikidataintegrator import wdi_core, wdi_fastrun, wdi_login
+from SPARQLWrapper import SPARQLWrapper, JSON
+from pprint import pprint
+from tqdm import tqdm
+import time
+import json
+import pickle
+import sys
+import os
+
+import src.utils as utils
+from src.format import format_cellosaurus_dump_as_dictionary
+from src.local import WDUSER, WDPASS
+from src.utils import *
+from src.query import *
+from src.wdi_wrapper import *
+from src.cellosaurusbot import *
+import requests
+import sys
+import pandas as pd
+
+
+def main():
+
+    # -----------------INPUT-------------------------#
+
+    cellosaurus_dump_path = sys.argv[1]
+    assert cellosaurus_dump_path, "You need to add a Cellosaurus Dump"
+
+    cellosaurus_dump_in_dictionary_format = format_cellosaurus_dump_as_dictionary(
+        cellosaurus_dump_path
+    )
+
+    url = "https://w.wiki/3Uxc"
+    session = requests.Session()  # so connections are recycled
+    resp = session.head(url, allow_redirects=True)
+    url_sparql = resp.url.replace(
+        "https://query.wikidata.org/#", "https://query.wikidata.org/sparql?query="
+    )
+    r = requests.get(url_sparql, params={"format": "json"})
+    df = pd.json_normalize(r.json()["results"]["bindings"])
+    print(df)
+    print(df.columns)
+    login = wdi_login.WDLogin(WDUSER, WDPASS)
+
+    df["qid"] = [url.split("/")[4] for url in df["item.value"]]
+
+    for cellosaurus_id in tqdm(cellosaurus_dump_in_dictionary_format):
+
+        if cellosaurus_id in df["cellosaurus.value"].values:
+            print("==========")
+            if (
+                cellosaurus_dump_in_dictionary_format[cellosaurus_id]["hPSCreg"]
+                == "NULL"
+            ):
+                print(f"Bad id for cell line {cellosaurus_id}")
+                data_to_add_to_wikidata = [
+                    wdi_core.WDBaseDataType.delete_statement("P9554")
+                ]
+                data_to_add_to_wikidata.append(
+                    wdi_core.WDExternalID(value=cellosaurus_id, prop_nr="P3289")
+                )
+
+                df_now = df[df["cellosaurus.value"] == cellosaurus_id]
+                properties_to_append_value = ["P3289"]
+                wd_item = wdi_core.WDItemEngine(
+                    wd_item_id=df_now["qid"].values[0],
+                    data=data_to_add_to_wikidata,
+                    append_value=properties_to_append_value,
+                )
+                a = wd_item.write(login, bot_account=True,)
+                print(a)
+
+
+if __name__ == "__main__":
+    main()
+
--- update_wikidata.py
+++ update_wikidata.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 
-
-'''
+"""
 Updates the Wikidata items relative to the cells in the Cellosaurus dump of interest. 
 
 It takes 3 arguments: 
@@ -14,7 +13,7 @@ were saved after running "prepare_files.py"
 
  Example:
  $python3 update_wikidata.py project/test_cellosaurus.txt dev/pickle_files dev/errors Q87574023 
- '''
+ """
 
 from wikidataintegrator import wdi_core, wdi_fastrun, wdi_login
 from SPARQLWrapper import SPARQLWrapper, JSON
@@ -34,10 +33,10 @@ from src.query import *
 from src.wdi_wrapper import *
 from src.cellosaurusbot import *
 
+
 def main():
 
-    #-----------------INPUT-------------------------#
-
+    # -----------------INPUT-------------------------#
 
     cellosaurus_dump_path = sys.argv[1]
     assert cellosaurus_dump_path, "You need to add a Cellosaurus Dump"
@@ -55,37 +54,43 @@ def main():
     wikidata_cell_lines_path = pickle_path + "/cell_lines_on_wikidata.pickle"
     filename_taxons = pickle_path + "/taxons_on_wikidata.pickle"
 
-    cellosaurus_dump_in_dictionary_format = format_cellosaurus_dump_as_dictionary(cellosaurus_dump_path)
+    cellosaurus_dump_in_dictionary_format = format_cellosaurus_dump_as_dictionary(
+        cellosaurus_dump_path
+    )
     cellosaurus_to_wikidata_matches = load_pickle_file(reconciled_dump_path)
-   
+
     wikidata_cell_lines = load_pickle_file(wikidata_cell_lines_path)
     login = wdi_login.WDLogin(WDUSER, WDPASS)
     ncbi_id_to_qid_species = load_pickle_file(filename_taxons)
-   
+
     references = cellosaurus_to_wikidata_matches["references_dictionary"]
     diseases = cellosaurus_to_wikidata_matches["diseases_dictionary"]
-  
+
     categories = load_cell_line_category_to_wikidata("project/category.txt")
-    
+
     for cellosaurus_id in tqdm(cellosaurus_dump_in_dictionary_format):
 
         tqdm.write(cellosaurus_id)
         try:
             tqdm.write(wikidata_cell_lines[cellosaurus_id])
-            cell_line = CellosaurusCellLine(wdi_login_object=login,
-                                        release_qid=release_qid,
-                                        cellosaurus_dump=cellosaurus_dump_in_dictionary_format,
-                                        wikidata_dictionary_with_existing_cell_lines=wikidata_cell_lines,
-                                        references=references,
-                                        species=ncbi_id_to_qid_species,
-                                        cell_line_categories=categories,
-                                        diseases=diseases,
-                                        cell_line_id=cellosaurus_id)
+            cell_line = CellosaurusCellLine(
+                wdi_login_object=login,
+                release_qid=release_qid,
+                cellosaurus_dump=cellosaurus_dump_in_dictionary_format,
+                wikidata_dictionary_with_existing_cell_lines=wikidata_cell_lines,
+                references=references,
+                species=ncbi_id_to_qid_species,
+                cell_line_categories=categories,
+                diseases=diseases,
+                cell_line_id=cellosaurus_id,
+            )
 
-            prepared_data =  cell_line.prepare_for_wikidata(folder_for_errors) 
+            prepared_data = cell_line.prepare_for_wikidata(folder_for_errors)
             cell_line.update_line_on_wikidata(prepared_data)
         except Exception as e:
             print(e)
 
-if __name__=="__main__": 
-    main() 
+
+if __name__ == "__main__":
+    main()
+
