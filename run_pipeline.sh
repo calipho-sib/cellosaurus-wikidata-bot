8821f6bbea6416b87df1a5967edfe14e9bcd824f
lubianat <tiago.lubiana.alves@usp.br>
2021-10-07 13:40:51 -0300

    Update references and logs

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -3,5 +3,6 @@ python3 prepare_files.py release_39/cellosaurus.txt pickle_files errors
 python3 add_articles.py errors
 python3 prepare_files.py release_39/cellosaurus.txt pickle_files errors
 
+python3 check_lines_on_wikidata.py release_39/cellosaurus.txt pickle_files errors Q108665770
 
-python3 check_lines_on_wikidata.py release_39/cellosaurus.txt pickle_files errors Q100993240
+python3 update_wikidata.py release_39/cellosaurus.txt pickle_files errors Q108665770
